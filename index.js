ab5bde27f54b22b8212fff395cdb8861d15e1bfd
alemranakash <alemranexpert@gmail.com>
2023-10-18 21:49:03 +0600

    Implement the capability to store data in a MongoDB database and retrieve data from it.

--- index.js
+++ index.js
@@ -29,6 +29,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productsCollection = client.db('productsDB').collection('products');
+
+    app.post('/products', async (req, res) => {
+      const newProducts = req.body;
+      console.log('newProducts', newProducts)
+      const result = await productsCollection.insertOne(newProducts)
+      res.send(result)
+  })
+
+  app.get('/products', async (req, res) => {
+    const result = await productsCollection.find().toArray();
+    res.send(result)
+})
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
